7c8437ff7230ec10741fb79fd532e982e0d17a4a
Mbongeni Nkosi <mbongenin114@gmail.com>
2025-04-29 10:21:54 +0200

    Bind schedules data to table in ViewScheduled page

---
+++ SmartCampusServices/ViewSchedules.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="ViewSchedules.aspx.cs" Inherits="SmartCampusServices.ViewSchedules" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <h2 class="mt-3 mb-3">View Schedules</h2>
+
+    <table id="scheduleTable" class="display table table-striped" style="width:100%">
+        <thead>
+            <tr>
+                <th>Class ID</th>
+                <th>Room</th>
+                <th>Day</th>
+                <th>Start Time</th>
+                <th>End Time</th>
+                <th>Course</th>
+                <th>Instructor</th>
+                <th>Year</th>
+                <th>Max Students</th>
+            </tr>
+        </thead>
+        <tbody>
+            <%-- You can bind rows using C# or server-side loop --%>
+            <asp:Literal ID="ltTableBody" runat="server" EnableViewState="false"></asp:Literal>
+        </tbody>
+    </table>
+
+    <script src="datatables/js/dataTables.min.js"></script>
+    <script type="text/javascript">
+        document.addEventListener("DOMContentLoaded", function () {
+            $('#scheduleTable').DataTable();
+        });
+    </script>
+</asp:Content>
---
+++ SmartCampusServices/ViewSchedules.aspx.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Npgsql;
+using System.Configuration;
+using System.Data;
+
+namespace SmartCampusServices
+{
+    public partial class ViewSchedules : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                BindScheduleTable();
+            }
+        }
+
+        private void BindScheduleTable()
+        {
+            string connStr = ConfigurationManager.ConnectionStrings["PostgresConnection"].ConnectionString;
+            using (NpgsqlConnection conn = new NpgsqlConnection(connStr))
+            {
+                string query = @"
+        SELECT cs.class_id, r.room_name, cs.day_of_week, cs.start_time, cs.end_time, 
+               c.course_name, u.full_name AS instructor, cs.academic_year, cs.max_students
+        FROM ""Class_Schedules"" cs
+        JOIN ""Course"" c ON cs.course_id = c.course_id
+        JOIN ""Rooms"" r ON cs.room_id = r.room_id
+        JOIN users u ON cs.instructor_id = u.id";
+
+                using (NpgsqlDataAdapter da = new NpgsqlDataAdapter(query, conn))
+                {
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+
+                    System.Text.StringBuilder sb = new System.Text.StringBuilder();
+                    foreach (DataRow row in dt.Rows)
+                    {
+                        sb.Append("<tr>");
+                        sb.AppendFormat("<td>{0}</td>", row["class_id"]);
+                        sb.AppendFormat("<td>{0}</td>", row["room_name"]);
+                        sb.AppendFormat("<td>{0:dddd}</td>", Convert.ToDateTime(row["day_of_week"]));
+                        sb.AppendFormat("<td>{0:HH:mm}</td>", row["start_time"]);
+                        sb.AppendFormat("<td>{0:HH:mm}</td>", row["end_time"]);
+                        sb.AppendFormat("<td>{0}</td>", row["course_name"]);
+                        sb.AppendFormat("<td>{0}</td>", row["instructor"]);
+                        sb.AppendFormat("<td>{0:yyyy}</td>", row["academic_year"]);
+                        sb.AppendFormat("<td>{0}</td>", row["max_students"]);
+                        sb.Append("</tr>");
+                    }
+
+                    ltTableBody.Text = sb.ToString();
+                }
+            }
+        }
+
+    }
+}
