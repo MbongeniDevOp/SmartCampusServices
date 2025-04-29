4a3628f26609ec50e7d1716cbe60743ade995e61
Mbongeni Nkosi <mbongenin114@gmail.com>
2025-04-29 15:48:27 +0200

    update homepage to reflect logged-in user state

--- SmartCampusServices/homepage.aspx.cs
+++ SmartCampusServices/homepage.aspx.cs
@@ -11,7 +11,23 @@ namespace SmartCampusServices
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                string fullName = Session["LoggedInFullName"]?.ToString();
+                if (!string.IsNullOrEmpty(fullName))
+                {
+                    LinkButton logout = (LinkButton)Master.FindControl("lnkLogout");
+                    LinkButton helloUser = (LinkButton)Master.FindControl("lnkHelloUser");
+                    LinkButton login = (LinkButton)Master.FindControl("lnkLogin");
+                    LinkButton viewSchedules = (LinkButton)Master.FindControl("lnkViewSchedules");
 
+                    login.Visible = false;
+                    viewSchedules.Visible = true;
+                    logout.Visible = true;
+                    helloUser.Visible = true;
+                    helloUser.Text = $"Hello, {fullName}";
+                }
+            }
         }
     }
 }
